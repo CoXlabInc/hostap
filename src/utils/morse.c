99c3192ada81963046f0a47833f3dded11ea4367
Jongsoo Jeong <jsjeong@coxlab.kr>
2026-03-09 12:31:26 +0900

    DK-13: Unify KR S1G bands into kr14 and kr15 classes
    Update KR band definitions to use a unified 917.5MHz start frequency, matching latest morse_driver changes and 802.11-2024 standard. This resolves frequency mismatch and ENOBUFS errors by including high-band channels (18, 19, etc.) in the standard operating classes.
    
    Assisted by Google Gemini

--- src/utils/morse.c
+++ src/utils/morse.c
@@ -400,21 +400,6 @@ static const struct ah_class jp12 = {
 	.chans = S1G_CHAN_ENABLED_FLAG(38),
 };
 
-static const struct ah_class kr13 = {
-	.s1g_freq_start = 920000,
-	.s1g_op_class = 13,
-	.s1g_op_class_idx = 13,
-	.global_op_class = 74,
-	.s1g_width = IEEE80211_CHAN_1MHZ,
-	.cc_list = {"KR"},
-	.chans = (
-		S1G_CHAN_ENABLED_FLAG(13) |  //926.5
-		S1G_CHAN_ENABLED_FLAG(15) |  //927.5
-		S1G_CHAN_ENABLED_FLAG(17) |  //928.5
-		S1G_CHAN_ENABLED_FLAG(19)    //929.5
-	),
-};
-
 static const struct ah_class kr14 = {
 	.s1g_freq_start = 917500,
 	.s1g_op_class = 14,
@@ -428,7 +413,11 @@ static const struct ah_class kr14 = {
 		S1G_CHAN_ENABLED_FLAG(5) |
 		S1G_CHAN_ENABLED_FLAG(7) |
 		S1G_CHAN_ENABLED_FLAG(9) |
-		S1G_CHAN_ENABLED_FLAG(11)
+		S1G_CHAN_ENABLED_FLAG(11) |
+		S1G_CHAN_ENABLED_FLAG(18) |
+		S1G_CHAN_ENABLED_FLAG(20) |
+		S1G_CHAN_ENABLED_FLAG(22) |
+		S1G_CHAN_ENABLED_FLAG(24)
 	),
 };
 
@@ -442,7 +431,9 @@ static const struct ah_class kr15 = {
 	.chans = (
 		S1G_CHAN_ENABLED_FLAG(2) |
 		S1G_CHAN_ENABLED_FLAG(6) |
-		S1G_CHAN_ENABLED_FLAG(10)
+		S1G_CHAN_ENABLED_FLAG(10) |
+		S1G_CHAN_ENABLED_FLAG(19) |
+		S1G_CHAN_ENABLED_FLAG(23)
 	),
 };
 
@@ -687,19 +678,6 @@ static const struct ah_class in31 = {
 	),
 };
 
-static const struct ah_class kr32 = {
-	.s1g_freq_start = 920000,
-	.s1g_op_class = 32,
-	.s1g_op_class_idx = 32,
-	.global_op_class = 75,
-	.s1g_width = IEEE80211_CHAN_2MHZ,
-	.cc_list = {"KR"},
-	.chans = (
-		S1G_CHAN_ENABLED_FLAG(14) |
-		S1G_CHAN_ENABLED_FLAG(18)
-	),
-};
-
 static const struct ah_class
 		*s1g_op_classes[] = {
 	NULL,
@@ -715,7 +693,7 @@ static const struct ah_class
 	&jp10,
 	&jp11,
 	&jp12,
-	&kr13,
+	NULL,
 	&kr14,
 	&kr15,
 	&kr16,
@@ -734,7 +712,6 @@ static const struct ah_class
 	&nz29,
 	&eu30,
 	&in31,
-	&kr32,
 };
 
 const int S1G_OP_CLASSES_LEN = ARRAY_SIZE(s1g_op_classes);
